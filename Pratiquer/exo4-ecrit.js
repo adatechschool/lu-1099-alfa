541a4afab3be8afe9f9452caebdcddf4483c9405
aliceKatkout <alice.katkout@gmail.com>
2021-02-16 15:39:50 +0100

    exo4

---
+++ Pratiquer/exo4-ecrit.js
@@ -0,0 +1,23 @@
+// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+// function reqListener () {
+//   console.log(this.responseText);
+// }
+// var req = new XMLHttpRequest();
+// req.responseType = 'json';
+// req.open('GET', '<https://www.affirmations.dev/>', true);
+// req.onload  = reqListener;
+// req.send(null);
+
+
+const fetch = require("node-fetch");
+
+const url = 'https://www.affirmations.dev';
+
+
+ async function getData(url) {
+   const data = await fetch(url)
+   const resp = await data.json()
+   console.log(resp);
+}
+
+getData(url)
