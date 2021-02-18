beeba258bb6d825d4d0b6e25e17ef5a77953e8cb
aliceKatkout <alice.katkout@gmail.com>
2021-02-18 10:36:12 +0100

    conflit resolu

--- Pratiquer/exo4-ecrit.js
+++ Pratiquer/exo4-ecrit.js
@@ -10,25 +10,15 @@
 
 
 const fetch = require("node-fetch");
-var resp_0 = [];
+
 const url = 'https://www.affirmations.dev';
 
 
  async function getData(url) {
    const data = await fetch(url)
    const resp = await data.json()
-<<<<<<< HEAD
    console.log(resp);
-   resp_0.push(resp);
 
 }
 
 getData(url)
-console.log(resp_0);
-=======
-   console.log(resp) ;
-}
-
-const result = getData(url);
-console.log(result);
->>>>>>> 294bcea33957d9b9db4ecc7c497bb64690a2eea8
